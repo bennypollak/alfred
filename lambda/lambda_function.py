c32f1685a0cfecc453e5c7a77e133f4c9bd2bfe9
Benny Pollak <yo372002@yahoo.com>
2024-07-12 11:32:36 -0400

    Refactor channel

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -48,12 +48,11 @@ class ScreenPowerIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return ask_utils.is_intent_name("ScreenPowerIntent")(handler_input)
     def handle(self, handler_input):
-        intent_name = get_intent_name(handler_input)
-        print(f"ScreenPowerIntent Intent Name: {intent_name}")
-        power, intended_power = utils.slot_value(handler_input, 'power', 'on')
         session_attributes = handler_input.attributes_manager.session_attributes
-        session_attributes['intent_name'] = intent_name
+        session_attributes['intent_name'] = get_intent_name(handler_input)
+        power, intended_power = utils.slot_value(handler_input, 'power', 'on')
         session_attributes['power'] = power
+        session_attributes['intended_power'] = intended_power
         room_name, intended_room_name = utils.slot_value(handler_input, 'room')
         if not room_name:
             room_name = intended_room_name = utils.room_echo_device_in(handler_input)
@@ -69,7 +68,6 @@ class ScreenPowerIntentHandler(AbstractRequestHandler):
         session_attributes['intended_room_name'] = intended_room_name
         return handle_request(handler_input, session_attributes)
 
-
 class RoomIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return ask_utils.is_intent_name("RoomIntent")(handler_input)
@@ -83,6 +81,35 @@ class RoomIntentHandler(AbstractRequestHandler):
             session_attributes['intended_room_name'] = intended_room_name
         return handle_request(handler_input, session_attributes)
 
+class ChannelIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return ask_utils.is_intent_name("ChannelIntent")(handler_input)
+    def handle(self, handler_input):
+        session_attributes = handler_input.attributes_manager.session_attributes
+        session_attributes['intent_name'] = get_intent_name(handler_input)
+        user_name = session_attributes['user_name']
+        channel_number, intended_channel_number = utils.slot_value(handler_input, 'channel_number')
+        station, intended_station = utils.slot_value(handler_input, 'station')
+        if not channel_number and not station:
+            speak_output = utils.alfred_voice("I didn't catch the channel")
+            return handler_input.response_builder.speak(speak_output).ask(speak_output).response
+        session_attributes['channel_number'] = channel_number
+        session_attributes['intended_station'] = intended_station
+        room_name, intended_room_name = utils.slot_value(handler_input, 'room')
+        if not room_name:
+            room_name = intended_room_name = utils.room_echo_device_in(handler_input)
+        if not room_name:
+            device_name = utils.echo_device_name(handler_input)
+            speak_output = utils.alfred_voice(f"channel for which room from {device_name}?")
+            return (handler_input.response_builder.speak(speak_output)
+                    .speak(speak_output)
+                    .add_directive(DelegateDirective(updated_intent=Intent(name="RoomIntent")))
+                    .response
+                    )
+        session_attributes['original_room_name'] = room_name
+        session_attributes['intended_room_name'] = intended_room_name
+        return handle_request(handler_input, session_attributes)
+
 class NextStepIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_intent_name("NextStepIntent")(handler_input)
@@ -90,35 +117,9 @@ class NextStepIntentHandler(AbstractRequestHandler):
         session_attributes = handler_input.attributes_manager.session_attributes
         original_room_name = session_attributes.get('original_room_name', 'unknown original room')
         intended_room_name = session_attributes.get('intended_room_name', 'unknown processed room')
-        speak_output = utils.alfred_voice(f"I will take care of {original_room_name} as {intended_room_name} for you Benny")
+        speak_output = utils.alfred_voice(f"I will take care of {original_room_name} as {intended_room_name}")
         return handler_input.response_builder.speak(speak_output).set_should_end_session(True).response
 
-class ChannelIntentHandler(AbstractRequestHandler):
-    def can_handle(self, handler_input):
-        return ask_utils.is_intent_name("ChannelIntent")(handler_input)
-    def handle(self, handler_input):
-        session_attributes = handler_input.attributes_manager.session_attributes
-        user_name = session_attributes['user_name']
-        room_name, intended_room_name = utils.slot_value(handler_input, 'room')
-        if not room_name:
-            speak_output = utils.alfred_voice("I didn't catch the room name. power for which room?")
-            return handler_input.response_builder.speak(speak_output).ask(speak_output).response
-        channel_number, intended_channel_number = utils.slot_value(handler_input, 'channel_number')
-        station, intended_station = utils.slot_value(handler_input, 'station')
-        if not channel_number and not station:
-            speak_output = utils.alfred_voice("I didn't catch the channel?")
-            return handler_input.response_builder.speak(speak_output).ask(speak_output).response
-        session_attributes['channel_number'] = channel_number
-        session_attributes['intended_station'] = intended_station
-        channel = channel_number if channel_number else station
-        print(f"room {room_name} {intended_room_name}  Channel: {channel} Channel Number: {channel_number} and intended channel number: {intended_channel_number}")
-        send_result = utils.send_channel_request(intended_room_name, channel_number, intended_station)
-        speak_output = utils.alfred_voice(f"{user_name}, I did {intended_room_name} channel to {channel} for {room_name} and got {send_result}")
-        print(speak_output)
-
-        return (handler_input.response_builder.speak(speak_output)
-                .set_card(SimpleCard(f"Alfred", f"Channel {channel} on {room_name}, got {send_result}"))
-                .set_should_end_session(True).response)
 
 def handle_request(handler_input, session_attributes):
     room_name = session_attributes.get('original_room_name')
